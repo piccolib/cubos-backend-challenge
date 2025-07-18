8b223f16d5226313bde8917d5178f2a0b4a2a6bc
bruno <bruno.piccoli@vlzm.com.br>
2025-07-18 17:51:38 -0300

    chore: initial project structure and EF Core setup

---
+++ CubosFinance.Persistence/Context/CubosFinanceDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CubosFinance.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CubosFinance.Persistence.Context;
+
+public class CubosFinanceDbContext : DbContext
+{
+    public CubosFinanceDbContext(DbContextOptions<CubosFinanceDbContext> options) : base(options) { }
+
+    public DbSet<Person> People { get; set; }
+
+}
